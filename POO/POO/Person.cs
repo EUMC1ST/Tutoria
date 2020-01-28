1be77ea48fb61cf9faf6e40939408e0d5f6ac248
Edgar Morales <edgar.morales@softtek.com>
2020-01-28 16:22:06 -0600

    Initial Project

---
+++ POO/POO/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO
+{
+    public class Person
+    {
+        public String Name { get; set;}
+        public int Edad { get; set;}
+        public String Address { get; set;}
+    }
+}
---
+++ POO/POO/Program.cs
@@ -0,0 +1,33 @@
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // The code provided will print ‘Hello World’ to the console.
+            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
+            Person person = new Person
+            {
+                Name = "Edgar",
+                Edad = 26,
+                Address = "Santa Catarina, Monterrey" 
+            };
+
+            string output = JsonConvert.SerializeObject(person);
+
+            FileStream fs = new FileStream(@"C:\Users\Curso\source\repos\Desktop\Tutoria\POO\POO\Sample.txt", FileMode.OpenOrCreate, FileAccess.Write);
+            using (StreamWriter writer = new StreamWriter(fs))
+            {
+                writer.Write(output);
+            }
+        }
+    }
+}
